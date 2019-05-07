394e5f9016aab7de27688ea3efc84e2a236a7ac6
hanqyu <peakjnut37@gmail.com>
2019-05-07 15:36:02 +0900

    어드민 추가

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from rangefilter.filter import DateTimeRangeFilter, DateRangeFilter
+from .models import *
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'username', 'last_login', 'created_at', 'region']
+    list_filter = ('region',
+        ('created_at', DateRangeFilter),
+        ('last_login', DateRangeFilter),
+    )
+    search_fields = ['id', 'email', 'username']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Region)
+admin.site.register(District)
+admin.site.register(Question)
+admin.site.register(Answer)
