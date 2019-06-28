d722c109afcd55a94fec12f399d0d780ca29e968
hanqyu <peakjnut37@gmail.com>
2019-06-28 13:52:35 +0900

    DEBUG FALSE

--- honeypot/settings.py
+++ honeypot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '.amazonaws.com', 'honeypot.hanqyu.com']
 
